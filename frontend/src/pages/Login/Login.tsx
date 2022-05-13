cd42bc55a9253ef1cf4fd943da268d8861701b50
fred-fittywebdev <contact@fittywebdev.fr>
2022-05-13 14:59:41 +0200

    Update login page, display error according to error message set in the backend

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,16 +1,20 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import React, { SyntheticEvent, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { NavLink, Redirect } from 'react-router-dom';
 import './Login.css';
 import { useCurrentUser } from '../../Context/UseCurrentUser';
+import { strictEqual } from 'assert';
 
 function Login() {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [userRole, setUserRole] = useState('');
 	const [redirect, setRedirect] = useState(false);
+	const [isValid, setIsValid] = useState(false);
+
+	type ServerError = { errorMessage: string };
 
 	const submit = async (e: SyntheticEvent) => {
 		e.preventDefault();
@@ -22,6 +26,7 @@ function Login() {
 			});
 			setRedirect(true);
 			setUserRole(data.user.is_admin);
+			setIsValid(data.user.is_valid);
 			// localStorage.setItem(
 			// 	'first_name',
 			// 	JSON.stringify(data.user.first_name)
@@ -32,9 +37,10 @@ function Login() {
 			console.log(data.token);
 			console.log(userRole);
 		} catch (error) {
-			if (error) {
-				toast.error('identifiants incorrects');
-				console.log(error);
+			const serverError = error as AxiosError;
+			if (serverError.response) {
+				console.log(serverError.response.data.message);
+				toast.warning(serverError.response.data.message);
 			}
 		}
 	};
